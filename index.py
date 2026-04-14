3d9267ce613388b94382dc6638b2d79ca3fa23c8
Anandhbasha <anadhakm@gmail.com>
2026-04-14 10:49:44 +0530

    pyt

---
+++ index.py
@@ -0,0 +1,66 @@
+# print("hello welcome",end="-")
+# print("hello","Welcome",sep="-")
+# hello-welcome
+
+# # "This python Base Class"
+#                 # -python
+
+# print("This python Base Class")
+# print("\t\tPython")
+# # apple/orange/banana
+
+# print("apple","orange","banana",sep="/")
+
+# a=10
+# print(a)
+# # left side a is called as variable
+# # = called as assignment opeartor
+# # 10 value
+# # print(a)
+# # print(a)
+# b=20
+# a=20.2
+# print(a)
+# # type
+# print(type(a))
+
+
+# a=10
+# b=20
+# c=30
+# a=20  b=30 c=10
+# temp = a
+# a=b
+# b=c
+# c= temp
+# print(a,b,c)
+
+# a,b,c = 10,20,30
+# c,a,b = a,b,c
+# print(a,b,c)
+
+# String
+# name ='a'
+# print(type(name))
+# bool
+# todayClass = True
+# print(type(todayClass))
+
+userName = "I love python"
+# index
+# index 0 =I
+# index 1 =
+# index 2 =l
+# index 3 =o
+# last index =-1
+# slicing
+# [startpoint:end point:increment]
+# startpoint
+# end point
+
+# I love pytho
+# Io t
+print(userName[::3])
+# nohtyp evol I
+# nhy vli 
+print(userName[-1::-2])
