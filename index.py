9a12f5ec8a8f8fdf6870195f6d5d859448ac2c7e
Anandhbasha <anadhakm@gmail.com>
2026-04-20 10:24:07 +0530

    dict

--- index.py
+++ index.py
@@ -78,38 +78,100 @@ userName = "I love python"
 
 # mid = len(s)//2
 
-# # pythonpr
-# # res = s[:mid][::-1]+s[mid:]
-# res = s[:mid]+s[mid:][::-1]
+# # # pythonpr
+# # # res = s[:mid][::-1]+s[mid:]
+# # res = s[:mid]+s[mid:][::-1]
+# # print(res)
+# # # 12. `"PythonProgramming"` → reverse only second half
+
+
+# # # 1234567890 get even index numbers
+# # # 0123456789
+# # values="1234567890"
+# # # # print(values[2::2])
+# # # print(values[1::2])
+
+# # print(values[1:-1])
+
+# # list
+# list = [10,30,20,40,10,40,30] 
+# # index = 0,1,2,3
+# # length = 4
+# list[1] = 60
+# print(list[1:2])
+# print(list[::-1])
+# print(list[1])
+# # tuple
+# newValue = (100,200,300,400,500)
+# # newValue[1] = 424
+# # print(newValue)
+# print(newValue[::-1])
+# # set
+# newSets = {101,101,101,102,102,103,108,105,104,106,107}
+
+# print(newSets)
+# res = tuple(list)
 # print(res)
-# # 12. `"PythonProgramming"` → reverse only second half
-
-
-# # 1234567890 get even index numbers
-# # 0123456789
-# values="1234567890"
-# # # print(values[2::2])
-# # print(values[1::2])
-
-# print(values[1:-1])
-
-# list
-list = [10,30,20,40,10,40,30] 
-# index = 0,1,2,3
-# length = 4
-list[1] = 60
-print(list[1:2])
-print(list[::-1])
-print(list[1])
-# tuple
-newValue = (100,200,300,400,500)
-# newValue[1] = 424
-# print(newValue)
-print(newValue[::-1])
-# set
-newSets = {101,101,101,102,102,103,108,105,104,106,107}
-
-print(newSets)
-res = tuple(list)
-print(res)
 # dictionary
+# newData = {
+#     "studentName":"Arun",
+#     "studentAge":30,
+#     "studentCity":"CBE",
+#     "studentCourse":"Python",
+#     "studentCompletedCourses":{
+#         "course1":"Java",
+#         "course2":"HTML",
+#         "course3":"Data Science"
+#     }
+# }
+
+# print(newData["studentCompletedCourses"]["course1"])
+
+# person = {
+#     "name":"ajay",
+#     "age":30,
+#     "city":"CBE",
+#     "family":{
+#         "father":"Raja",
+#         "mother":"Lakshmi",
+#         "siblings":{
+#             "brothers":["Arun","Vijay","Ravi"],
+#             "sister":"kala"
+#         }
+#     }
+# }
+# print(person["family"]["siblings"]["brothers"])
+
+persons = [
+{
+    "name":"ajay",
+    "age":30,
+    "city":"CBE",
+    "family":{
+        "father":"Raja",
+        "mother":"Lakshmi",
+        "siblings":{
+            "brothers":["Arun","Vijay","Ravi"],
+            "sister":"kala"
+        }
+    }
+},
+{
+    "name":"vijay",
+    "age":30,
+    "city":"CBE",
+    "family":{
+        "father":"Muthu",
+        "mother":"Kamala",
+        "siblings":{
+            "brothers":["Bala","arun","Rahul"],
+            "sister":"Deepa"
+        }
+    }
+}
+]
+
+
+print(persons[1]["family"]["siblings"]["brothers"])
+
+# json
