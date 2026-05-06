ed460175626b2b4be15f33335dc526a3af5f7d63
Anandhbasha <anadhakm@gmail.com>
2026-05-06 10:52:00 +0530

    while

---
+++ loops.py
@@ -0,0 +1,54 @@
+# while
+# while condition:
+    # code to exectue
+
+# num1 = int(input("Enter the number"))
+
+# while num1 <=10:
+#     print(num1)
+#     num1+=1
+
+# even numbers
+# while num1<=10:
+#     if num1%2==0:
+#         print(num1)
+#     num1+=1
+# 1*3=3
+# 2*3=6
+# 3*3=9
+# 4*3=12
+# while num1<=10:
+#     print(num1, "*" ,3,"=",num1*3 )
+#     num1+=1
+
+# a=0
+# while a<5:
+#     print("Hello Welcome")
+#     a+=1
+
+# a=0
+# while a<=100:
+#     a+=5
+#     print(a)
+# n = int(input("Enter the number:"))
+# while n<=100:
+#     if n%5==0:
+#         print(n)
+    # n+=1
+
+# oddOrEven = int(input("Enter the Number to find Odd or Even:"))
+# while oddOrEven<=100:
+#     if oddOrEven%2!=0:
+#         print("Green")
+#     else:
+#         print("Red")
+#     oddOrEven+=1
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+i=10
+while i>=5:
+    print(i)
+    i-=1
+# for
