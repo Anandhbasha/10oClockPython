e038c263cb895542e0b649627d4c7b07b7b1309c
Anandhbasha <anadhakm@gmail.com>
2026-05-11 10:32:50 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,70 @@
+# def functionname():
+    # statement
+
+# s1T = 70
+# s1E = 90
+# s1M = 77
+# s1s = 66
+# s1SS = 82
+
+# s2T = 70
+# s2E = 90
+# s2M = 77
+# s2s = 66
+# s2SS = 82
+
+
+# print("Hello Welcome")
+# print("Hello Welcome")
+# print("Hello Welcome")
+# print("Hello Welcome")
+# print("Hello Welcome")
+# def newPrint():
+#     print("Hello Welcome")
+
+# newPrint()
+# newPrint()
+# newPrint()
+# newPrint()
+# newPrint()
+
+
+# def addMarks(t,e,m,s,ss):
+#     print(t+e+m+s+ss)
+
+# addMarks(60,70,80,90,95)
+# addMarks(66,77,82,90,95)
+# # addMarks(66,87,82,90,95)
+
+
+
+# def addMarks(*args):
+#     # *varible to list convert
+#     total=0
+#     for i in args:
+#         total+=i
+#     print(total)
+# addMarks(60,70,80,90,95)
+# addMarks(60,70,80,90)
+# addMarks(60,70,95)
+
+
+# def fixedShow(a=10,b=20):
+#     print(a,b)
+
+# fixedShow()
+
+
+
+# def fixedShow(a,b):
+#     print(a,b)
+
+# fixedShow(b=60,a=70)
+
+# lamda functions
+
+square = lambda x: x**x
+print(square(2))
+
+square = lambda x,y: x+y
+print(square(2,4))
--- loops.py
+++ loops.py
@@ -47,8 +47,112 @@
 # while i<=10:
 #     print(i)
 #     i+=1
-i=10
-while i>=5:
-    print(i)
-    i-=1
+# i=10
+# while i>=5:
+#     print(i)
+#     i-=1
 # for
+# for variable in seqence:
+#     statement
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(11):
+# i=0
+# i=1
+# i=2
+
+# for i in range(11):
+#     if i%2==0:
+#         print(i)
+
+# for i in range(11):
+#     if i%2!=0:
+#         print(i)
+
+# for i in range(1,13):
+# # range(start,next of endValue)
+#     print(i)
+
+# for i in range(1,13,2):
+#     # range(start,next of endValue,step)
+#     print(i)
+
+# for i in range(10,-1,-2):
+#     print(i)
+
+# 0 to 120
+# for i in range(0,121,3):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i,"*",5,"=", i*5)
+
+userName = "Arjun"
+
+# for i in userName:
+#     print(i)
+
+# arr = ["Apple","kiwi","orange"]
+
+# for i in arr:
+#     print(i)
+
+
+# 0to6
+# 0+1
+# 1+2
+# 3+3
+# 6+4
+# 10+5
+# 15
+
+# sum of number
+# 1 to 8
+# 1+1
+# 2+2
+# 4+3
+# 7+4
+# 11+5
+# 16+6
+# 22+7
+# 29
+total = 1
+for i in range(1,8):
+    total+=i
+print(total)
+
+# for loop repeated work
+# range numbers generate
+# i temporary variable
+
+# *
+# # **
+# # ***
+# # ****
+# # *****
+
+# # for i in range(1,6):
+# #     print(i*"*")
+
+# # vals = "are"
+# # count = 0
+# # for i in vals:
+# #     if i in "aeiouAEIOU":
+# #         count+=1
+# # print(count)
+
+
+
+# name = "bala"
+
+# for i in name:
+#     if i in 'a':
+#         print("Hello")
+#     # i=b
+#     # a
+#     # l
+#     # a
+#     # print(i)
+
